ff5197d802e3f1b812f2ac00f9d93e21254878ab
Aleksandr Sotnikov <93768828+alexandrsotnickov@users.noreply.github.com>
2023-10-27 00:39:47 +0700

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,6 @@
+SELECT products.name, categories.name
+FROM products
+LEFT JOIN product_categories
+ON products.Id = product_categories.product_id
+LEFT JOIN categories
+ON product_categories.category_id = categories.id;
