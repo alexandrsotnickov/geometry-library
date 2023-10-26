e2c07fa5fd5edead07c41c057ec27676623c9ea6
Aleksandr Sotnikov <93768828+alexandrsotnickov@users.noreply.github.com>
2023-10-27 00:46:12 +0700

    added renaming "null" to "no category"

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-SELECT products.name, categories.name
+SELECT products.name, ISNULL(categories.category_name, 'No category') as category_name
 FROM products
 LEFT JOIN product_categories
 ON products.id = product_categories.product_id
